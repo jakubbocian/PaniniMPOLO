388b7c8a02003c6af313743636cac58766f7d678
jakubbocian <jakub.bocian@mpolo.it>
2022-12-11 17:34:51 +0100

    generazione id

--- registrazione/controlloEmail.php
+++ registrazione/controlloEmail.php
@@ -1,26 +1,29 @@
 <?php
-//prendo l'email
-$email = $_REQUEST['email'];
-
-//importo il file csv
-$fp = fopen("dB_marcopolo.csv", "r");
-//creo un array con i dati del file csv
-$dati = array();
-while ($line = fgetcsv($fp, 1000, ";")) {
-    $dati[] = $line;
-}
-fclose($fp);
-
-$risposta = "email non presente";
-
-//controllo se l'email è già presente nel file csv
-for($i=0; $i<count($dati); $i++){
-    if($dati[$i][4] == $email){
-        $risposta = "email presente";
-        break;
+function getAlunni() {
+    $dati = array();
+    $fp = fopen("db_marcopolo.csv", "r");
+    while ($line = fgetcsv($fp, 1000, ";")) {
+        $dati[] = $line;
     }
+    fclose($fp);
+    return $dati;
 }
 
-//invio risposta
-echo $risposta;
+
+function checkEmail($email) {
+    $dati = getAlunni();
+    $fp = fopen("db_marcopolo.csv", "r");
+    while ($line = fgetcsv($fp, 1000, ";")) {
+        $dati[] = $line;
+    }
+    fclose($fp);
+    $risposta = false;
+    for($i=0; $i<count($dati); $i++){
+        if($dati[$i][4] == $email){
+            $risposta = true;
+            break;
+        }
+    }
+    return $risposta;
+}
 ?>
