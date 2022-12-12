960fe5db0333d984b525892556b704dd1ddda625
jakubbocian <jakub.bocian@mpolo.it>
2022-12-12 18:10:41 +0100

    controllo mail nel db

--- registrazione/registrazione.php
+++ registrazione/registrazione.php
@@ -3,23 +3,44 @@ require 'controlloEmail.php';
 
 $email = $_GET['email'];
 
-//genero un id univoco
-function uniqidReal($lenght = 6) {
-    if (function_exists("random_bytes")) {
-        $bytes = random_bytes(ceil($lenght / 2));
-    } elseif (function_exists("openssl_random_pseudo_bytes")) {
-        $bytes = openssl_random_pseudo_bytes(ceil($lenght / 2));
-    } 
-    return substr(bin2hex($bytes), 0, $lenght);
+//connessione al db
+function OpenCon()
+{
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "ciao";
+    $db = "MPolo";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
+
+    return $conn;
 }
 
-//controllo email
-if(checkEmail($email)){
-    $id = uniqidReal();
-    $hashedId = password_hash($id, PASSWORD_DEFAULT);
-    $hashedIdLenght = strlen($hashedId);
-    echo $id . " " . $hashedId . " " . $hashedIdLenght;
-} else {
-    echo "email non valida";
+function CloseCon($conn)
+{
+    $conn->close();
 }
+
+
+$conn = OpenCon();
+
+//controllo se l'email è nel dominio
+if (checkEmail($email)){
+    //controllo se l'email è già presente nel db
+    $sql = "SELECT * FROM utente WHERE email = '$email'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo "Email già presente nel database";
+    } else {
+        echo "Email non presente" . "<br>";
+        $sql = "INSERT INTO `utente` (`email`) VALUES ('$email')";
+        $result = $conn->query($sql);
+        echo "Email inserita nel database";
+    }
+} else {
+    echo "Email non presente nel dominio";
+}
+
+
+CloseCon($conn);
 ?>
