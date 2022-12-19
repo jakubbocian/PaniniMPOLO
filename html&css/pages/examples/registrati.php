04417d9ce349f8f3c42daeb53f9821d45c93ec53
jakubbocian <jakub.bocian@mpolo.it>
2022-12-19 17:50:04 +0100

    Inizio invio mail

--- html&css/pages/examples/registrati.php
+++ html&css/pages/examples/registrati.php
@@ -1,39 +1,71 @@
 <?php
 require 'controlloEmail.php';
+require 'sendEmail.php';
 
 $email = $_GET['email'];
 
-//connessione al db
+/*
+    * Apertura connessione al database
+    * In caso di errore viene creato un popup di errore terminato lo script
+    * @return mysqli
+*/
 function OpenCon()
 {
     $dbhost = "localhost";
     $dbuser = "root";
     $dbpass = "ciao";
+<<<<<<< Updated upstream
     $db = "MPolo";
 
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
+=======
+    $db = "my_iismpsnacks";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
+    if($conn->connect_errno){
+        echo json_encode(array('popUpName' => 'popDanger', 'title' => "Errore", 'caption' => 'Connessione interrotta, errore nel database!'));
+        exit();
+    }
+>>>>>>> Stashed changes
     return $conn;
 }
 
+/*
+    * Chiusura connessione al database
+    * @param mysqli $conn
+*/
 function CloseCon($conn)
 {
     $conn->close();
 }
 
+<<<<<<< Updated upstream
 
 $conn = OpenCon();
 
 //controllo se l'email è nel dominio
 if (checkEmail($email)){
     //controllo se l'email è già presente nel db
+=======
+/*
+    SCRIPT PRINCIPALE
+*/
+
+// Controllo se l'email è presente nel dominio
+if (checkEmail($email)){
+    $conn = OpenCon();
+>>>>>>> Stashed changes
     $sql = "SELECT * FROM utente WHERE email = '$email'";
     $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
+    // Controllo se l'email è già presente nel database
+    if ($result->num_rows > 0) {  
         echo json_encode(array('popUpName' => 'popWarning', 'title' => "Attenzione", 'caption' => 'Email già presente nel database'));
+    // Invio email per l'attivazione dell'account
     } else {
-        $sql = "INSERT INTO `utente` (`email`) VALUES ('$email')";
-        $result = $conn->query($sql);
+        /*$sql = "INSERT INTO `utente` (`email`) VALUES ('$email')";
+        $result = $conn->query($sql);*/
+        sendEmail($email);
         echo json_encode(array('popUpName' => 'popSuccess', 'title' => "Ok", 'caption' => "Entro pochi minuti riceverai una mail di conferma all'indirizzo specificato. Controlla anche la cartella di spam"));
     }
 } else {
